21e27de0d1db6d478de1683af6ed21829b19ec7a
紫幻 <zihuancz@foxmail.com>
2018-06-13 17:45:05 +0800

    添加生成二维码

--- QrCodeLib2/app/src/main/java/com/qrcode/app/MainActivity.java
+++ QrCodeLib2/app/src/main/java/com/qrcode/app/MainActivity.java
@@ -1,21 +1,65 @@
 package com.qrcode.app;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.qrcode.app.zxing.MipcaActivityCapture;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.img_qr);
     }
 
     public void erweima(View view) {
         startActivity(new Intent(this, MipcaActivityCapture.class));
+    }
+
+    public void buildqr(View view) {
+        imageView.setImageBitmap(generateQRCode("https://github.com/zihuan1/AndroidLibrary"));
+    }
+
+    // 生成二维码
+    private Bitmap bitMatrix2Bitmap(BitMatrix matrix) {
+        int w = matrix.getWidth();
+        int h = matrix.getHeight();
+        int[] rawData = new int[w * h];
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                int color = Color.WHITE;
+                if (matrix.get(i, j)) {
+                    color = Color.BLACK;
+                }
+                rawData[i + (j * w)] = color;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+        bitmap.setPixels(rawData, 0, w, 0, 0, w, h);
+        return bitmap;
+    }
+
+    private Bitmap generateQRCode(String content) {
+        try {
+            QRCodeWriter writer = new QRCodeWriter();
+            // MultiFormatWriter writer = new MultiFormatWriter();
+            BitMatrix matrix = writer.encode(content, BarcodeFormat.QR_CODE, 500, 500);
+            return bitMatrix2Bitmap(matrix);
+        } catch (WriterException e) {
+        }
+        return null;
     }
 }
